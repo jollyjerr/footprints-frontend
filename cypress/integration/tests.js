dae9886f3ac65d053d453e3750455f1fdc0052f4
jollyjerr <jollyjerr@gmail.com>
2019-10-08 16:25:03 -0600

    inits testing

--- cypress/integration/tests.js
+++ cypress/integration/tests.js
@@ -1,7 +1,11 @@
 describe("Unit Testing", () => {
-    
+
 })
 
 describe("E2E", () => {
-
+    describe('Landing Page', () => {
+        it('loads without crashing', () => {
+            cy.visit('/')
+        }) 
+    })  
 })
