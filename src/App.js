b07ac2c62247804b2e403ccb735d4c7f884ce3ab
jollyjerr <jollyjerr@gmail.com>
2019-10-17 09:48:57 -0600

    WIP: redux refactor

--- src/App.js
+++ src/App.js
@@ -1,69 +1,65 @@
 import './App.scss'
 
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
+import React from 'react'
+import {connect} from 'react-redux'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import LandingContainer from './containers/LandingContainer'
 import HomeContainer from './containers/HomeContainer'
 import NoMatch from './components/NoMatch'
 
-
-
-export default class App extends Component {
+function App(props) {
   BACKED_URL = "http://localhost:5000/api"
-  state = {
-    user: {},
-  }
 
-  isloggedIn = () => {
-    if(Object.keys(this.state.user).length !== 0) {
-      return true
-    }
-    let jwt = localStorage.getItem("footprintsJWT")
-    return jwt ? true : false
-  }
+  return (
+    <Router>
 
-  logIn = (formData) => {
-    fetch(`${this.BACKEND_URL}/auth/login`, {
-      "headers": {
-        "Content-Type": "application/json"
-      },
-      "body": JSON.stringify({
-        username: formData.username,
-        password: formData.password
-      })
+      <Navbar />
+
+      <Switch>
+
+        <Route exact path="/">
+          {this.isloggedIn() ? <Redirect to="/home" /> : <LandingContainer login={this.logIn} />}
+        </Route>
+
+        <Route exact path ="/home">
+          <HomeContainer />
+        </Route>
+
+         <Route component={NoMatch} />
+      </Switch>
+
+    </Router>
+  )
+}
+
+const logIn = formData => {
+  fetch(`${this.BACKEND_URL}/auth/login`, {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: formData.username,
+      password: formData.password
     })
+  })
     .catch(alert)
     .then(resp => resp.json())
     .then(userData => {
       this.setState({
         user: userData
-      })
+      });
     })
-    .catch(alert)
+    .catch(alert);
+};
+
+const isloggedIn = () => {
+  if (Object.keys(this.state.user).length !== 0) {
+    return true;
   }
+  let jwt = localStorage.getItem("footprintsJWT");
+  return jwt ? true : false;
+};
 
-  render() {
-    return (
-      <Router>
-
-        <Navbar />
-
-        <Switch>
-
-          <Route exact path="/">
-            {this.isloggedIn() ? <Redirect to="/home" /> : <LandingContainer login={this.logIn} />}
-          </Route>
-
-          <Route exact path ="/home">
-            <HomeContainer />
-          </Route>
-
-          <Route component={NoMatch} />
-        </Switch>
-
-      </Router>
-    )
-  }
-}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
